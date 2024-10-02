214f9eaa9626aaea7b52f5f6391dd69bdecd669e
Manoel Carlo Bertin <manoelcarlobertin@outlook.com>
2024-10-02 11:12:31 -0300

    A validacao qdo a data final for depois da inicial.

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,6 +7,10 @@ class Event < ApplicationRecord
 
   private
 
+  def validate_if_finished_at_is_after_started_at
+    errors.add(:finished_at, "must be after started_at") if started_at.present? && finished_at.present? && finished_at < started_at
+  end
+
   def validate_if_starts_in_the_future
     errors.add(:started_at, "must be in the future") if started_at.present? && started_at < Time.zone.now
   end
