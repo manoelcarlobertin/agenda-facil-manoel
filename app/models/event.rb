e81f3b8f5c1c0fbdd31ed938c1f8f44ca40adeb2
Manoel Carlo Bertin <manoelcarlobertin@outlook.com>
2024-10-02 11:05:58 -0300

    Refinando minhas validacoes utilizando IA Cody.

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,11 @@ class Event < ApplicationRecord
   validates :description, presence: true, length: { minimum: 2, maximum: 500, allow_blank: true }
   validates :started_at, presence: true
   validates :finished_at, presence: true
+  validate :validate_if_starts_in_the_future, on: :create
+
+  private
+
+  def validate_if_starts_in_the_future
+    errors.add(:started_at, "must be in the future") if started_at.present? && started_at < Time.zone.now
+  end
 end
