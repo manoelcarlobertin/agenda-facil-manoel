74e3ddfdfd797335d148de85ecd900f12e86f6cb
Manoel Carlo Bertin <manoelcarlobertin@outlook.com>
2024-10-02 13:39:24 -0300

    Tenho o nome da tabela seguido do nome da coluna ou das colunas em [].

--- db/migrate/20241002162431_create_categories.rb
+++ db/migrate/20241002162431_create_categories.rb
@@ -7,6 +7,9 @@ class CreateCategories < ActiveRecord::Migration[7.2]
 
       t.timestamps
     end
-    add_index :categories, :indentifier, unique: true
+    # Add unique index to the identifier column, pssing the table name and column name as arguments;
+    # The unique: true option ensures that don't two records can have the same identifier;
+    # I can also use more than one column in the index by passing [ an array ] of column names;
+    add_index :categories, [ :indentifier ], unique: true
   end
 end
