cef4ecabc83fdd5c1375a3f1a2ed4de8dac9ff24
Manoel Carlo Bertin <manoelcarlobertin@outlook.com>
2024-10-04 10:25:32 -0300

    Quis nome genério até pq pode ser p/ exemplo comprovante pgto.

--- app/models/event.rb
+++ app/models/event.rb
@@ -12,7 +12,7 @@ class Event < ApplicationRecord
   validate :validate_if_starts_in_the_future, on: :create
   validate :validate_if_finished_at_is_after_started_at
 
-  has_one_attached :image
+  has_one_attached :file
 
   private
 
